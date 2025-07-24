50caa9a152227e90fcde3a0a90b2fd60537935b1
Ahmed Farag <farag0336@gmail.com>
2025-07-25 00:05:34 +0300

    first commit

---
+++ src/main/java/GitHub_Actions_CI_CD_pipeline/CI_CD_pipeline/CiCdPipelineApplication.java
@@ -0,0 +1,13 @@
+package GitHub_Actions_CI_CD_pipeline.CI_CD_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GitHub_Actions_CI_CD_pipeline/CI_CD_pipeline/CiCdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package GitHub_Actions_CI_CD_pipeline.CI_CD_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
